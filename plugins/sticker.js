9c091917046da7401c3ba38bcec169fe6e6e4c9d
shefin <96108399+shefin-x3@users.noreply.github.com>
2022-09-09 12:10:21 +0530

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -7,10 +7,10 @@ bot(
     desc: "_Converts Photo or video to sticker_",
     type: "converter",
   },
-  async (message, match,m) => {
+  async (message, match, m) => {
     if(!(message.reply_message.video||message.reply_message.image)) return await message.reply('_Reply to photo or video_')
     let buff = await m.quoted.download()
-    message.sendMessage(buff,{ packname: config.PACKNAME, author: config.AUTHOR },'sticker')
+    message.sendMessage(buff, { packname: config.STICKER_PACKNAME.split(';')[0], author: config.STICKER_PACKNAME.split(';')[1] }, 'sticker')
   }
 );
 
