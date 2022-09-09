44173882a3d6454ec32ba9c81531e3b5ebc48347
shefin <96108399+shefin-x3@users.noreply.github.com>
2022-09-09 14:49:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -123,6 +123,22 @@ conn.sendMessage(data.id,{image:{url:userpp},caption:msg.replace(/{pp}/,''),ment
           }
           events.commands.map(async (command) => {
             if (command.pattern.test(text_msg)) {
+
+                    let sendMsg = false;
+                    var chat = conn.chats.get(msg.key.remoteJid)
+                        
+                    if ((config.SUDO !== false && msg.key.fromMe === false && command.fromMe === true &&
+                        (msg.participant && config.SUDO.includes(',') ? config.SUDO.split(',').includes(msg.participant.split('@')[0]) : msg.participant.split('@')[0] == config.SUDO || config.SUDO.includes(',') ? config.SUDO.split(',').includes(msg.key.remoteJid.split('@')[0]) : msg.key.remoteJid.split('@')[0] == config.SUDO)
+                    ) || command.fromMe === msg.key.fromMe || (command.fromMe === false && !msg.key.fromMe)) {
+                        if (command.onlyPinned && chat.pin === undefined) return;
+                        if (!command.onlyPm === chat.jid.includes('-')) sendMsg = true;
+                        else if (command.onlyGroup === chat.jid.includes('-')) sendMsg = true;
+                    }
+              if (sendMsg) {
+                        if (config.SEND_READ && command.on === undefined) {
+                            await conn.readMessages(msg.key.remoteJid);
+                        }
+
               var match = text_msg.match(command.pattern)[1] || false;
               whats = new Message(conn, msg, ms);
 
